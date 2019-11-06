8118d1815e77cc81dc0f29156c98a5fdbae452cb
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-11-06 14:36:57 -0300

    Separando actions

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,14 +1,31 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 // * é um generator, uma função do Javascript equivalente ao async
 function* addToCart({ id }) {
-    // yield equivalente ao await
-    const response = yield call(api.get, `/products/${id}`);
+    const productExits = yield select(state =>
+        state.cart.find(p => p.id === id)
+    );
 
-    yield put(addToCartSuccess(response.data));
+    if (productExits) {
+        const amount = productExits.amount + 1;
+
+        yield put(updateAmount(id, amount));
+    } else {
+        // yield equivalente ao await
+        const response = yield call(api.get, `/products/${id}`);
+
+        const data = {
+            ...response.data,
+            amount: 1,
+            priceFormatted: formatPrice(response.data.price),
+        };
+
+        yield put(addToCartSuccess(data));
+    }
 }
 
 // takeLatest executa o ultimo clique do usuario
